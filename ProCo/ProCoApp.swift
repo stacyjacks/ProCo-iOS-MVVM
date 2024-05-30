f66d8db921fc3a141153931ac26bf4b6ce27adcb
Anastasia K <34249502+stacyjacks@users.noreply.github.com>
2024-05-30 11:36:30 +0200

    Initial Commit

---
+++ ProCo/ProCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProCoApp.swift
+//  ProCo
+//
+//  Created by Anastasia on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ProCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
